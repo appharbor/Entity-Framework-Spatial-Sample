70603065d25da344b106c532cfabdee0e95de9fa
Michael Friis <friism@gmail.com>
2012-09-17 20:48:57 -0700

    remove gml stuff

--- Web/Controllers/LoadController.cs
+++ Web/Controllers/LoadController.cs
@@ -1,8 +1,4 @@
-﻿using Services;
-using System.Data.Spatial;
-using System.Linq;
-using System.Web.Mvc;
-using System.Xml.Linq;
+﻿using System.Web.Mvc;
 
 namespace Web.Controllers
 {
@@ -10,17 +6,7 @@ namespace Web.Controllers
 	{
 		public ActionResult Index()
 		{
-			var kmlDocument = XDocument.Load("http://geo.oiorest.dk/politikredse/12/grænse.kml");
-			var geometry = kmlDocument.Descendants("{http://earth.google.com/kml/2.2}MultiGeometry").Single();
-			
-			geometry.SetNamespace("http://opengis.net/gml");
-
-			var gmlDocument = XDocument.Parse(geometry.ToString());
-
-			var geography = DbGeography.FromGml(gmlDocument.ToString());
-
 			return Content(string.Format("District parsed"));
 		}
-
 	}
 }
