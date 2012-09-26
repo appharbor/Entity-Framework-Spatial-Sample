64e121be73c15b630f7230859d8a23cfb5c114f3
Michael Friis <friism@gmail.com>
2012-09-25 19:22:24 -0700

    add seeding

--- Core/Persistence/Configuration.cs
+++ Core/Persistence/Configuration.cs
@@ -1,4 +1,10 @@
-﻿using System.Data.Entity.Migrations;
+﻿using Core.Model;
+using Services.DataLoad.ShapeFiles;
+using System;
+using System.Data.Entity.Migrations;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Core.Persistence
 {
@@ -8,6 +14,23 @@ namespace Core.Persistence
 		{
 			AutomaticMigrationsEnabled = true;
 			AutomaticMigrationDataLossAllowed = true;
+		}
+
+		protected override void Seed(Context context)
+		{
+			if (!context.Districts.Any())
+			{
+				var location = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+				var dataPath = Path.Combine(Path.GetDirectoryName(location), "data", "KOMMUNE");
+				var shapes = new DagiShapeFileReader().Read(dataPath, "KOMNAVN", "DAGI_ID");
+				foreach (var shape in shapes)
+				{
+					context.Districts.Add(new District { Name = shape.Key, Geography = shape.Value });
+				}
+				context.SaveChanges();
+			}
+
+			base.Seed(context);
 		}
 	}
 }
