8ef14e8f3d11d149aa4f16048b3c237dd09540ef
Michael Friis <friism@gmail.com>
2012-09-25 19:51:54 -0700

    remove error handling

--- Web/App_Start/FilterConfig.cs
+++ Web/App_Start/FilterConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Web
 {
@@ -7,7 +6,6 @@ namespace Web
 	{
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
-			filters.Add(new HandleErrorAttribute());
 		}
 	}
 }
